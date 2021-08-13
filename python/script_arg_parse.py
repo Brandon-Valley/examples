cb05a0d54ce7d7be02a814d09d41ca40a740292c
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2021-08-13 10:33:32 -0500

    updated python args

--- python/script_arg_parse.py
+++ python/script_arg_parse.py
@@ -7,6 +7,7 @@ import time
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--do_file_name'               , default = 'run_cmd.do')
 parser.add_argument('-r', '--run_to_pane_shift_sleep_sec', default = 4) # 7
+parser.add_argument('-t','--true_or_false_flag_example', action='store_true') # true if add flag to cmd line, false if don't
 args = parser.parse_args()
 
 time.sleep(1)
