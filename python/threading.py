1263e993d020723d3a56752625c122943c202f1e
Brandon-Valley <bavvh8@mst.edu>
2023-01-16 15:09:09 -0600

    Create threading.py

---
+++ python/threading.py
@@ -0,0 +1,23 @@
+# https://superfastpython.com/threadpoolexecutor-wait-all-tasks/
+
+# SuperFastPython.com
+# example of waiting for tasks to complete
+from time import sleep
+from random import random
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import wait
+ 
+# custom task that will sleep for a variable amount of time
+def task(name):
+    # sleep for less than a second
+    sleep(random())
+    print(f'Done: {name}')
+ 
+# start the thread pool
+with ThreadPoolExecutor(2) as executor:
+    # submit tasks and collect futures
+    futures = [executor.submit(task, i) for i in range(10)]
+    # wait for all tasks to complete
+    print('Waiting for tasks to complete...')
+    wait(futures)
+    print('All tasks are done!')
