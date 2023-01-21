1ff19aa2a04860fcd0531def862e0954b20faa36
Brandon-Valley <bavvh8@mst.edu>
2023-01-21 07:48:11 -0600

    Update threading.py

--- python/threading.py
+++ python/threading.py
@@ -6,18 +6,23 @@ from time import sleep
 from random import random
 from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import wait
- 
+ 
 # custom task that will sleep for a variable amount of time
 def task(name):
-    # sleep for less than a second
-    sleep(random())
-    print(f'Done: {name}')
- 
+    # sleep for less than a second
+    sleep(random())
+    print(f'Done: {name}')
+ 
 # start the thread pool
 with ThreadPoolExecutor(2) as executor:
-    # submit tasks and collect futures
-    futures = [executor.submit(task, i) for i in range(10)]
-    # wait for all tasks to complete
-    print('Waiting for tasks to complete...')
-    wait(futures)
-    print('All tasks are done!')
+    futures = []
+
+    for i in range(10):
+        
+        # submit tasks and collect futures
+        futures = [executor.submit(task, i)]
+
+    # wait for all tasks to complete
+    print('Waiting for tasks to complete...')
+    wait(futures)
+    print('All tasks are done!')
