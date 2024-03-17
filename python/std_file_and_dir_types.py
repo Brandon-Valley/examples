b5216eb3c277a200fa438a50183da5ec4208ffff
Brandon-Valley <bvalley111@gmail.com>
2024-03-17 10:30:02 -0500

    Create std_file_and_dir_types.py

---
+++ python/std_file_and_dir_types.py
@@ -0,0 +1,35 @@
+# Standard Python File & Directory Types
+
+## File Types
+
+---
+
+## "Main" files
+
+Examples:
+
+- main.py
+- cli.py
+- gui.py
+
+## Single function implementation files
+
+TODO
+
+## Class implementation files
+
+TODO
+
+## Tools files?
+
+Repo-specific
+
+## Util files
+
+Not repo-specific
+
+- Ideally always in some std dependency for easy reuse
+
+## __init__ files
+
+EMPTY
