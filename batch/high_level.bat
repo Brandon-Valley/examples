61669463261e73d91ff8fdff1de7007d48cee7e5
Brandon-Valley <bvalley111@gmail.com>
2024-07-04 15:45:47 -0500

    Update high_level.bat

--- batch/high_level.bat
+++ batch/high_level.bat
@@ -3,3 +3,10 @@ setlocal enabledelayedexpansion
 
 REM Keep cmd window open until script finishes in case of error
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
+
+REM Get path to parent of this script (with trailing /)
+set SCRIPT_PARENT_DIR=%~dp0
+
+REM Run example script using path relative to this script
+PowerShell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
+PowerShell -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ""%SCRIPT_PARENT_DIR%wifi_fix.ps1""' -Verb RunAs"
